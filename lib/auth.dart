ec46c82f49fd1c4f0b9f824516834aa57718173e
Manohar Acharya <manoharacharya63@gmail.com>
2023-10-01 17:42:29 +0530

    3.auth methods

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable, unused_catch_clause
+
 import 'package:appwrite/appwrite.dart';
 
 //Appwrite Initilisation
@@ -5,4 +7,47 @@ Client client = Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('65192055e6e15fcbc9e5')
     .setSelfSigned(
-        status: true); // For self signed certificates, only use for development
+      status: true,
+    ); // For self signed certificates, only use for development
+
+//Sign Up
+Account account = Account(client);
+
+Future<String> createUser(String name, String email, String password) async {
+  try {
+    final user = await account.create(
+      userId: ID.unique(),
+      name: '',
+      email: '',
+      password: '',
+    );
+    return 'sucess';
+  } on AppwriteException catch (e) {
+    return e.message.toString();
+  }
+}
+
+//Login
+Future loginUser(String email, String password) async {
+  try {
+    final user =
+        await account.createEmailSession(email: email, password: password);
+  } on AppwriteException catch (e) {
+    return false;
+  }
+}
+
+//Logout
+Future logoutUser() async {
+  await account.deleteSession(sessionId: 'current');
+}
+
+//Check if any user have an active session
+Future checkSession() async {
+  try {
+    await account.getSession(sessionId: 'current');
+    return true;
+  } on AppwriteException catch (e) {
+    return false;
+  }
+}
